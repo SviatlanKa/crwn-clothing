5866b649533f655d01421ac0e867da7ce248f2a7
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-11-14 20:59:09 +0300

    add react-router-dom components

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import { sections } from "./Directory.data";
 import MenuItem from '../menu-item/MenuItem';
 import './Directory.scss';
