1a0400abbb97613762a817c9594622aa43e946f8
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-12-17 17:59:31 +0300

    added directory reducer and selector, modified Directory component

--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CollectionPreview from '../../components/collection-preview/CollectionPreview';
-import { SHOP_DATA} from './shop.data';
+import { SHOP_DATA} from '../../redux/shop/shop.data';
 
 class ShopPage extends Component {
     constructor(props) {
