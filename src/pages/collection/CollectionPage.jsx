42da31541797988d2916359be09a9a004753a8d7
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-12-18 17:55:14 +0300

    added Collection page and nested routing in Shop page

---
+++ src/pages/collection/CollectionPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CollectionItem from '../../components/collection-item/CollectionItem';
+import './CategoryPage.scss';
+
+const CollectionPage = () => (
+    <div className="collection-page">
+        <h2>CATEGORY PAGE</h2>
+    </div>
+);
+
+export default CollectionPage;
