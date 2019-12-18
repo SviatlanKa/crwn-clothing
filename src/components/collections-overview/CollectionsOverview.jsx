45a5d90ec226bdf9c2092f8c532556f601524464
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-12-18 11:46:43 +0300

    added CollectionOverview component

---
+++ src/components/collections-overview/CollectionsOverview.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopCollections } from '../../redux/shop/shop-selectors';
+import CollectionPreview from '../collection-preview/CollectionPreview';
+import './CollectionsOverview.scss';
+
+const CollectionsOverview = ({ collections }) => (
+    <div className="collection-overview">
+        <h1>Collections</h1>
+        {
+            collections.map(({ id, ...otherCollectionsProps }) => (
+                <CollectionPreview key={id} { ...otherCollectionsProps } />
+            ))
+        }
+    </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview)
--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,29 +1,10 @@
-import React, { Component } from 'react';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
-import { SHOP_DATA} from '../../redux/shop/shop.data';
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                <h1>Collections</h1>
-                {
-                    collections.map(({ id, ...otherCollectionsProps }) => (
-                        <CollectionPreview key={id} { ...otherCollectionsProps } />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const ShopPage = () => (
+    <div className="shop-page">
+        <CollectionsOverview />
+    </div>
+);
 
 export default ShopPage;
