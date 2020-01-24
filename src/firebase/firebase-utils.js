a92c83121136f7aaac362ecc9508b9097f7de86b
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2020-01-24 17:38:14 +0300

    create react-redux-saga branch before convert all code to React Hooks

--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -45,16 +45,17 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 };
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-    const collectionRef = firestore.collection(collectionKey);
-    const batch = firestore.batch();
-
-    objectsToAdd.forEach(obj => {
-        const newDocRef = collectionRef.doc();
-        batch.set(newDocRef, obj);
-    });
-    return await batch.commit();
-};
+//function for export collections to firebase
+// export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+//     const collectionRef = firestore.collection(collectionKey);
+//     const batch = firestore.batch();
+//
+//     objectsToAdd.forEach(obj => {
+//         const newDocRef = collectionRef.doc();
+//         batch.set(newDocRef, obj);
+//     });
+//     return await batch.commit();
+// };
 
 export const convertCollectionsSnapshotToMap = collections => {
     const transformedCollection = collections.docs.map(doc => {
